3138e5dac1c76fbaa138a7baf6ba38d4698702af
yousof <fosuoy@hotmail.com>
2017-11-19 12:45:09 +0000

    Added template for problems

---
+++ problem_template.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import time
+
+def blurb():
+  print("""
+  blurb
+  """)
+
+def main():
+
+  start = time.time()
+
+  RESULT = ""
+
+  end   = time.time() - start
+
+  print("Result: %s"      % RESULT)
+  print("Completed in %s" % end)
+
+if __name__ == '__main__':
+  main()
