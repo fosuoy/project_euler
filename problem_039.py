205bdce1cd1ca8a07046b589fd00ec097386680d
yousof <fosuoy@hotmail.com>
2017-12-26 22:18:17 +0000

    Added problem 039

---
+++ problem_039.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+import time
+
+def blurb():
+    print("""
+If p is the perimeter of a right angle triangle with integral length sides, {a,b,c}, there are exactly three solutions for p = 120.
+
+{20,48,52}, {24,45,51}, {30,40,50}
+
+For which value of p ≤ 1000, is the number of solutions maximised?
+    """)
+
+def find_solutions_for_triangle(number, powers):
+    upper_bound = number - 2
+    results = []
+    products = set()
+    test_range = range(1,upper_bound)
+    for a in test_range:
+        for b in test_range[::-1]:
+            if b > a:
+                a_2 = powers[a]
+                b_2 = powers[b]
+                c_2 = powers[a] + powers[b]
+                try:
+                    c = powers.index(c_2)
+                    if a + b + c == number and c not in products:
+                        products.add(c)
+                        results.append([a, b, c])
+                except ValueError:
+                    pass
+    return results
+
+def find_most_results_for_range(upper_bound):
+    upper_bound += 1
+    powers_list = [x ** 2 for x in range(0,upper_bound)]
+    result = 0
+    result_length = 0
+    test_range = [x for x in range(1, upper_bound) if x % 12 == 0]
+    for number in test_range:
+        solutions = find_solutions_for_triangle(number, powers_list)
+        if len(solutions) > result_length:
+            result = number
+            result_length = len(solutions)
+            print(str(number) + ":")
+            print(solutions)
+    return result
+
+def main():
+    '''
+    Speed is:
+    Result: 840
+    Completed in 282.286678314209
+
+    '''
+    blurb()
+    start = time.time()
+    RESULT = find_most_results_for_range(1000)
+    end   = time.time() - start
+    print("Result: %s"      % RESULT)
+    print("Completed in %s" % end)
+
+if __name__ == '__main__':
+    main()
