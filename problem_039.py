86997d775adc73117e1e4c1cd9347f29a4b46447
yousof <fosuoy@hotmail.com>
2017-12-29 12:01:38 +0000

    Modified problem 039

--- problem_039.py
+++ problem_039.py
@@ -11,37 +11,33 @@ If p is the perimeter of a right angle triangle with integral length sides, {a,b
 For which value of p ≤ 1000, is the number of solutions maximised?
     """)
 
-def find_solutions_for_triangle(number, powers):
+def find_solutions_for_triangle(number, powers, power_set):
     upper_bound = number - 2
     results = []
-    products = set()
     middle_bound = upper_bound // 2
     quarter_bound = middle_bound // 2
     test_range_lower = range(1, quarter_bound)
     test_range_high = range(quarter_bound, middle_bound)
     for a in test_range_lower:
         for b in test_range_high:
-            if b > a:
-                a_2 = powers[a]
-                b_2 = powers[b]
-                c_2 = powers[a] + powers[b]
-                try:
-                    c = math.sqrt(c_2)
-                    if a + b + c == number and c not in products:
-                        products.add(c)
-                        results.append([a, b, c])
-                except ValueError:
-                    pass
+            a_2 = powers[a]
+            b_2 = powers[b]
+            c_2 = a_2 + b_2
+            if c_2 in power_set:
+                c = powers.index(c_2)
+                if a + b + c == number:
+                    results.append([a, b, c])
     return results
 
 def find_most_results_for_range(upper_bound):
     upper_bound += 1
     powers_list = [x ** 2 for x in range(0,upper_bound)]
+    power_set = set(powers_list)
     result = 0
     result_length = 0
-    test_range = [x for x in range(1, upper_bound) if x % 12 == 0]
+    test_range = [x for x in range(1, upper_bound)]
     for number in test_range:
-        solutions = find_solutions_for_triangle(number, powers_list)
+        solutions = find_solutions_for_triangle(number, powers_list, power_set)
         if len(solutions) > result_length:
             result = number
             result_length = len(solutions)
