3dcb19c423f76a2ccb2b36cd439e5f01977c4f84
yousof <fosuoy@hotmail.com>
2019-08-04 09:03:05 +0100

    python/problem_template - Updated to use a decorator

--- python/problem_template.py
+++ python/problem_template.py
@@ -2,20 +2,34 @@
 import time
 
 
+def problem_output(func, *args):
+    def wrapper(*args):
+        blurb()
+        start = time.time()
+        result = func(*args)
+        time_to_end = time.time() - start
+        print(f"Result: {result}")
+        print(f"Completed in {time_to_end}")
+
+    return wrapper
+
+
 def blurb():
-    print("""
+    print(
+        """
     blurb
-    """)
+    """
+    )
+
+
+@problem_output
+def problem():
+    return 0
 
 
 def main():
-    blurb()
-    start = time.time()
-    RESULT = ""
-    end   = time.time() - start
-    print("Result: %s"      % RESULT)
-    print("Completed in %s" % end)
+    problem()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
